1d4ee47853019dea9de17d941d0cadf07cef733e
Saksham <sakshamhorecah@gmail.com>
2024-06-08 13:11:49 +0530

    Global CSS and cards animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Clients from './components/Clients'
